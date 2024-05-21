660101eb3883fc401ccb9c7c81e92d6d9e8f6df8
Syed Hasnain <www.hasnainabbas96@gmail.com>
2024-05-21 12:53:33 +0500

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do-List By: Syed Hasnain</title>
+    <style>
+
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            background-color: #f5f5f5;
+        }
+
+        header {
+            background-color: #007bff;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+
+    
+       .project-section {
+            max-width: 600px;
+            margin: 40px auto;
+            padding: 20px;
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+
+        footer {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+
+<header>
+    <h1>Laravel 10 CRUD (Create, Read, Update and Delete)</h1>
+</header>
+
+<main>
+<section class="project-section">
+    <h2><a href="http://127.0.0.1:8000/student">Go to the 127.0.0.1:8000/students</a></h2>
+</section>
+
+</main>
+
+<footer>
+    <p>&copy; 2024 Syed Hasnain. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
